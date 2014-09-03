3408389da79bfb82497d69e417d880152be55353
wxppt <wx_ppt@126.com>
2014-09-04 00:50:56 +0800

    算是把图片的那块写好了

--- manager-message/index.js
+++ manager-message/index.js
@@ -6,39 +6,79 @@ $(document).ready(function() {
     
     $(".msgreply").click(function() {
         $(this).hide();
-        $(this).parents(".messagerow").animate({"height":"430px"});
-        $(this).parents(".messagerow").children(".msgmain").children(".replypanel").show();
+        $(this).parents(".msgrow").animate({"height": $(this).parents(".msgrow").height() + 260 + "px"});
+        $(this).parents(".msgrow").children(".msgmain").animate({"height": $(this).parents(".msgrow").children(".msgmain").height() + 260 + "px"});
+        $(this).parents(".msgrow").children(".msgmain").children(".replypanel").show();
     });
 
     $(".cancelreply").click(function() {
-        $(this).parents(".messagerow").animate({"height":"180px"}, function() {
+        $(this).parents(".msgrow").children(".msgmain").animate({"height": $(this).parents(".msgrow").children(".msgmain").height() - 260 + "px"});
+        $(this).parents(".msgrow").animate({"height": $(this).parents(".msgrow").height() - 260 + "px"}, function() {
             $(this).children(".msgmain").children(".msgbar").children(".msgreply").show();
             $(this).children(".msgmain").children(".replypanel").hide();
         });
     });
-
-    setMsgDetailHeight();
 });
 
-var zoomPic = function(pic) {
-    var aspect = $(pic).width() / $(pic).height();
-    var wMargin = parseInt($(pic).parents(".msgdetail").width() * 0.8);
-    $(pic).parents(".msgmain").children(".replypanel").hide();
 
-    $(pic).animate({"height": wMargin / aspect + "px"});
-    $(pic).parents(".messagerow").animate({"height": wMargin / aspect + $(pic).parents(".messagerow").height() - 120 + "px"}, function() {
-        setMsgDetailHeight();
-        $(pic).parents(".msgmain").children(".replypanel").show();
+
+var zoomPic = function(pic) {
+    // 计算宽高比
+    var aspect = $(pic).width() / $(pic).height();
+    // 计算最宽限制
+    var wMargin = parseInt($(pic).parents(".msgdetail").width() * 0.8);
+    var addHeight = parseInt(wMargin/aspect - $(pic).height());
+    // 隐藏回复框
+    var $replyPanel = $(pic).parents(".msgmain").children(".replypanel");
+    var isReplying = false;
+    if($replyPanel.css("display") != "none") {
+        isReplying = true;
+        $(pic).parents(".msgmain").children(".replypanel").hide();
+    }
+    // 对msgpic picmsgdetail picmsgmain picmsgrow 增加相同的高度
+    $(pic).animate({"height": $(pic).height() + addHeight + "px"});
+    $(pic).parents(".picmsgdetail").css({"height": $(pic).parents(".picmsgdetail").height() + addHeight + "px"});
+    $(pic).parents(".picmsgmain").css({"height": $(pic).parents(".picmsgmain").height() + addHeight + "px"});
+    $(pic).parents(".picmsgrow").animate({"height": $(pic).parents(".picmsgrow").height() + addHeight + "px"},function() {
+        // 还原回复框
+        if(isReplying) {
+            $(pic).parents(".msgmain").children(".replypanel").show();
+        }
         $(pic).attr("title","Click to zoom out");
         $(pic).unbind("click");
         $(pic).click(function() {
             shrinkPic(this);
         });
     });
-   
 }
 
 var shrinkPic = function(pic) {
+    var shrinkHeight = parseInt($(pic).height() - 120);
+
+    // 隐藏回复框
+    var $replyPanel = $(pic).parents(".msgmain").children(".replypanel");
+    var isReplying = false;
+    if($replyPanel.css("display") != "none") {
+        isReplying = true;
+        $(pic).parents(".msgmain").children(".replypanel").hide();
+    }
+    // 对msgpic picmsgdetail picmsgmain picmsgrow 增加相同的高度
+    $(pic).animate({"height": $(pic).height() - shrinkHeight + "px"});
+    $(pic).parents(".picmsgdetail").animate({"height": $(pic).parents(".picmsgdetail").height() - shrinkHeight + "px"});
+    $(pic).parents(".picmsgmain").animate({"height": $(pic).parents(".picmsgmain").height() - shrinkHeight + "px"});
+    $(pic).parents(".picmsgrow").animate({"height": $(pic).parents(".picmsgrow").height() - shrinkHeight + "px"},function() {
+        // 还原回复框
+        if(isReplying) {
+            $(pic).parents(".msgmain").children(".replypanel").show();
+        }
+        $(pic).attr("title","Click to zoom in");
+        $(pic).unbind("click");
+        $(pic).click(function() {
+            zoomPic(this);
+        });
+    });
+
+
     $(pic).animate({"height": "120px"});
     $(pic).parents(".messagerow").animate({"height": "180px"}, function() {
         setMsgDetailHeight();
@@ -48,12 +88,4 @@ var shrinkPic = function(pic) {
             zoomPic(this);
         });
     });
-}
-
-var setMsgDetailHeight = function() {
-    var pic = $(".msgpic")[0];
-    while($(pic).length > 0) {
-        $(pic).parents(".msgdetail").height($(pic).height());
-        pic = $(pic).next();
-    }
 }
